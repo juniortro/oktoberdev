267f86f03e908aca3b036b92de6d07caa542905c
Carlos <carlos@cloudpark.com.br>
2022-10-05 14:07:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let bonusButton = document.getElementById("bonus")
+let footer = document.getElementById("footer")
+let audio = document.getElementById("audio")
+
+bonusButton.addEventListener("click", () => {
+    footer.classList.add("show")
+    footer.classList.remove("hidden")
+    bonusButton.classList.add("hidden")
+    audio.play()
+    window.scrollTo({
+        top: 0,
+    })
+})
+
+
